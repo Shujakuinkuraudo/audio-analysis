47177dacf0544a11bed833da84c4b96abb095d9d
YunyaoZhou <admin@shujk.top>
2024-05-27 22:27:50 +0800

    ckpt

--- models/CNN.py
+++ models/CNN.py
@@ -34,7 +34,6 @@ class CNN(nn.Module):
     def __init__(self, num_classes = 7):
         super(CNN, self).__init__()
         self.MFCC_TOTAL2VECTOR = nn.Sequential(
-            nn.LazyBatchNorm2d(),
             nn.Conv2d(1, 16, 3, padding="same"),
             nn.ReLU(),
             nn.LazyBatchNorm2d(),
