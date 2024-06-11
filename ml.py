8eeb1c93688bfa1beac545f7f9b185ff1ffca36a
YunyaoZhou <admin@shujk.top>
2024-06-11 20:00:47 +0800

    chore: Update ml.py to include additional classifiers and use kfold cross-validation

--- ml.py
+++ ml.py
@@ -12,20 +12,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 clfs = [SVC(C=0.1,gamma="auto"), XGBClassifier(), DecisionTreeClassifier(), KNeighborsClassifier(), RandomForestClassifier()]
+feature_cache = {}
+emodb_fold, emodb_labels = emodb_fold_ml(fold = 5, feature_cache= feature_cache)
+savee_fold, savee_labels = savee_fold_ml(fold = 4, feature_cache= feature_cache)
 
 for clf in clfs:
     run = wandb.init(project='audio analysis', name=f"kfold - all - {repr(clf)}", reinit=True)
-    feature_cache = {}
 
     try:
         tp = Train_process()
-        emodb_fold, emodb_labels = emodb_fold_ml(fold = 5, feature_cache= feature_cache)
         acc,accs = tp.test_fold(emodb_fold, clf, emodb_labels)
         run.log({"emodb - 5 - acc": acc})
         for i in range(5):
             run.log({f"emodb_{i}_acc": accs[i]})
 
-        savee_fold, savee_labels = savee_fold_ml(fold = 4, feature_cache= feature_cache)
         acc,accs = tp.test_fold(savee_fold, clf, savee_labels)
         run.log({"savee - 4 - acc": acc})
         for i in range(4):
