7fb792d4fb59cbf88f94441152e6fe1a34d4cfb9
YunyaoZhou <admin@shujk.top>
2024-06-12 08:54:17 +0800

    CNN

--- models/CNN.py
+++ models/CNN.py
@@ -38,16 +38,16 @@ from .TIMNET import TIMNET, WeightLayer
 class CNN(nn.Module):
     def __init__(self, num_classes = 7):
         super(CNN, self).__init__()
-        # self.MFCC_TOTAL2VECTOR = nn.Sequential(
-        #     nn.LazyConv2d(64, (5,5), padding=2),
-        #     nn.ReLU(),
-        #     nn.LazyBatchNorm2d(),
-        #     ResidualBlock(64, 64),
-        #     ResidualBlock(64, 64),
-        #     nn.AdaptiveAvgPool2d((8, 8)),
-        #     nn.Flatten(),
-        #     nn.LazyLinear(128)
-        # )
+        self.MFCC_TOTAL2VECTOR = nn.Sequential(
+            nn.LazyConv2d(64, (5,5), padding=2),
+            nn.ReLU(),
+            nn.LazyBatchNorm2d(),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            nn.AdaptiveAvgPool2d((8, 8)),
+            nn.Flatten(),
+            nn.LazyLinear(128)
+        )
         # self.MFCC_PARTIAL2VECTOR = nn.Sequential(
         #     nn.LazyConv2d(64, (5,5), padding=2),
         #     nn.ReLU(),
@@ -65,9 +65,10 @@ class CNN(nn.Module):
         #     nn.ReLU(),
         #     nn.LazyLinear(num_classes),
         # )
-        self.TIMNET = TIMNET(nb_filters=39, kernel_size=2, nb_stacks=1, dilations=None, activation="relu", dropout_rate=0.1, return_sequences=True)
-        self.WEIGHTLAYER = WeightLayer()
+        # self.TIMNET = TIMNET(nb_filters=39, kernel_size=2, nb_stacks=1, dilations=None, activation="relu", dropout_rate=0.1, return_sequences=True)
+        # self.WEIGHTLAYER = WeightLayer()
         self.classifier = nn.Sequential(
+            nn.ReLU(),
             nn.LazyLinear(num_classes),
         )
 
@@ -75,7 +76,7 @@ class CNN(nn.Module):
         return nn.functional.cross_entropy(y, target)
     
     def forward(self, mfcc_total, mfcc_partial=None):
-        # x = self.MFCC_TOTAL2VECTOR(mfcc_total).unsqueeze(1) # b, seq_len, feature
+        x = self.MFCC_TOTAL2VECTOR(mfcc_total.unsqueeze(0)) # b, seq_len, feature
         # mfcc_partial b,seq_len,channel,width,height
         # b,seq_len,channel,widht,height = mfcc_partial.size()
         # mfcc_partial = mfcc_partial.view(b*seq_len, channel, widht, height)
@@ -84,14 +85,14 @@ class CNN(nn.Module):
         # x_partial, _ = self.RNN(x_partial)
         # x, _ = self.Attention(x, x_partial, x_partial)
         # x = x.squeeze(1)
-        x = self.TIMNET(mfcc_total)
-        x = self.WEIGHTLAYER(x)
+        # x = self.TIMNET(mfcc_total)
+        # x = self.WEIGHTLAYER(x)
 
         return self.classifier(x) 
 
         
 if __name__ == "__main__":
     ae = CNN()
-    x = torch.randn(32, 1, 13, 201)
+    x = torch.randn(30, 13, 201)
     y = ae(x)
     print(y.shape)
